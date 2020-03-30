ebcfe7f80a95f7753e0a2519d47496a17a663ca3
Jethet <henriette.hettinga@gmail.com>
2020-03-30 10:38:50 +0200

    Create App, .env and secret API_KEY

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from "react";
+require("dotenv").config();
 
-const App = () => <div>This is the app</div>;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const url = `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo`;
+
+class App extends React.Component {
+
+  handleGetRequest = () => {
+    console.log('working')
+  }
+
+  componentDidMount() {
+    this.handleGetRequest() // this keyword refers to the App component due to use of arrow function
+  }
+
+  render() {
+    return (
+    <div>App Component</div>
+    )
+  }
+}
 
 export default App;
